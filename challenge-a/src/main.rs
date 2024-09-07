5b89b17c9860436652837716b2398b8a6e817678
Shreethaar <shreethaara380@gmail.com>
2024-09-07 16:25:14 +0000

    [ZNTfuAd2yNfjH]

--- challenge-a/src/main.rs
+++ challenge-a/src/main.rs
@@ -1,3 +1,7 @@
+#![allow(unused)]
+
+const BIRTHDAY: i32 = 1;
+
 fn main() {
     /* Challenge 1 - Build a program that has the following:
 
@@ -7,6 +11,13 @@ fn main() {
     4) Has a local mutable integer variable named 'age' with your current age as the value
     5) Has a local integer variable named 'new_age' with your age after your birthday as the value
     6) Prints out 'My name is X and I am X years old. I will turn X on X' 
-
     */
+
+    let my_name="Shree";
+    let my_birthday="September 11";
+    let mut age = 69;
+    let new_age = age+BIRTHDAY;
+
+    println!("My name is {} and I am {} years old. I will turn {} on {}.",my_name,age,new_age,my_birthday);
+
 }
